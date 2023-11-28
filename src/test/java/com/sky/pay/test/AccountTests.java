6079a035e9ef135240c28266580c892f04103592
gabidabet <gabida.steam@gmail.com>
2023-11-28 22:35:41 +0100

    create DepositShouldIncreaseBalance test

---
+++ src/test/java/com/sky/pay/test/AccountTests.java
@@ -0,0 +1,14 @@
+package com.sky.pay.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountTests {
+
+	@Test
+	public void DepositShouldIncreaseBalance() {
+		fail("Not yet implemented");
+	}
+
+}
