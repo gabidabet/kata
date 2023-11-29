2a9d2d9bafc45dbce872eff4e91708a73eb694d0
gabidabet <gabida.steam@gmail.com>
2023-11-29 01:13:48 +0100

    add printStatementTests

--- src/main/java/com/sky/pay/test/Account.java
+++ src/main/java/com/sky/pay/test/Account.java
@@ -1,13 +1,26 @@
 package com.sky.pay.test;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Account implements AccountService {
 
 	private int balance;
 	private PrintInerface printInerface;
+	private static final int PADDING = 12;
+	private static final String PATTERN_FORMAT = "dd/MM/yyyy";
+	private final DateTimeFormatter formatter;
+	private final List<Operation> operations;
 	
 	public Account(int initialBalance, PrintInerface printInerface) {
 		balance = initialBalance;
 		this.printInerface = printInerface;
+		formatter = DateTimeFormatter.ofPattern(PATTERN_FORMAT)
+				.withZone(ZoneId.of("UTC"));
+		operations = new ArrayList<>();
 	}
 	
 	@Override
@@ -16,6 +29,7 @@ public final class Account implements AccountService {
 			return;
 		}
 		balance += amount;
+		operations.add(new Operation(Instant.now(), amount, balance));
 	}
 
 	@Override
@@ -27,15 +41,79 @@ public final class Account implements AccountService {
 			return;
 		}
 		balance -= amount;
+		operations.add(new Operation(Instant.now(), -amount, balance));
 	}
 
 	@Override
 	public void printStatement() {
-		printInerface.print("");
+		StringBuilder statementBuilder = new StringBuilder();
+		AppendRow(statementBuilder, "Date", "Amount", "Balance");
+		for(int i = operations.size() - 1; i >= 0; i--) {
+			AppendRow(statementBuilder,
+					formatter.format(operations.get(i).getDate()),
+					operations.get(i).getAmount() + "",
+					operations.get(i).getBalance() + "");
+		}
+		
+		printInerface.print(statementBuilder.toString());
 		
 	}
 	
 	public int getBalance() {
 		return balance;
 	}
+	
+	private void AppendRow(StringBuilder statementBuilder,
+			String dateToken,
+			String amountToken,
+			String balanceToken) {
+		final String separator = "|| ";
+		statementBuilder.append(padRight(dateToken, ' ', PADDING))
+				.append(separator)
+				.append(padRight(amountToken, ' ', PADDING))
+				.append(separator)
+				.append(padRight(balanceToken, ' ', PADDING))
+				.append('\n');
+	}
+	
+	private String padRight(String inputString, char caractere, int length) {
+	    if (inputString.length() >= length) {
+	        return inputString;
+	    }
+	    StringBuilder sb = new StringBuilder();
+	    sb.append(inputString);
+	    while (sb.length() < length) {
+	        sb.append(caractere);
+	    }
+
+	    return sb.toString();
+	}
+	
+
+	public final class Operation {
+		private Instant date;
+		private int amount;
+		private int balance;
+		
+		public Operation(Instant date, int amount, int balance) {
+			super();
+			this.date = date;
+			this.amount = amount;
+			this.balance = balance;
+		}
+		
+		public Instant getDate() {
+			return date;
+		}
+		
+		public int getAmount() {
+			return amount;
+		}
+		
+		public int getBalance() {
+			return balance;
+		}
+	}
 }
+
+
--- src/test/java/com/sky/pay/test/PrintStatementTests.java
+++ src/test/java/com/sky/pay/test/PrintStatementTests.java
@@ -14,7 +14,34 @@ class PrintStatementTests {
 		PrintInerface mock = mock(PrintInerface.class);
 		Account service = new Account(0, mock);
 		String expectedOutput = new StringBuilder()
-				.append("Date 				|| Amount				|| Balance						")
+				.append("Date        || Amount      || Balance     ")
+				.append("\n")
+				.toString();
+		
+		// Act
+		service.printStatement();
+		
+		// Assert
+		verify(mock, times(1)).print(expectedOutput);
+	}
+	
+	@Test
+	void printStatementShouldPrintAllOperationOnAccount() {
+		// Arrange
+		PrintInerface mock = mock(PrintInerface.class);
+		Account service = new Account(0, mock);
+		service.deposit(1000);
+		service.deposit(2000);
+		service.withdraw(500);
+		String expectedOutput = new StringBuilder()
+				.append("Date        || Amount      || Balance     ")
+				.append("\n")
+				.append("29/08/2023  || -500        || 2500        ")
+				.append("\n")
+				.append("29/08/2023  || 2000        || 3000        ")
+				.append("\n")
+				.append("29/08/2023  || 1000        || 1000        ")
+				.append("\n")
 				.toString();
 		
 		// Act
