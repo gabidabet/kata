7670c4db334a78fed6e41ad63ac1a6500a405ff8
gabidabet <gabida.steam@gmail.com>
2023-11-29 01:57:38 +0100

    main

--- src/main/java/com/sky/pay/test/Main.java
+++ src/main/java/com/sky/pay/test/Main.java
@@ -2,6 +2,14 @@ package com.sky.pay.test;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		AccountService service = new Account(0,
+				content -> System.out.println(content),
+				new CustumTimePicker());
+		service.deposit(1000);
+		service.deposit(23000);
+		service.withdraw(0);
+		service.withdraw(500);
+		service.withdraw(1000);
+		service.printStatement();
 	}
 }
